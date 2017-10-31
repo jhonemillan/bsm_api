795339b5f51c02678ec9e75bb240719d8a1dd14a
Jhon Edwin Millan <jemillan@parmac.local>
2017-10-31 14:31:25 -0500

    api para track

---
+++ index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+var port = process.env.port | 3000;
+
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.set('view engine','html');
+
+
+app.get('*',function(req, res){
+    console.log('entra al get');
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
+app.post('/track', (req, res)=>{
+    SendCoords(req.body.lat, req.body.lon);
+    res.end();
+})
+
+function SendCoords(lat, lon) {
+    // Not the movie transporter!
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'jhonemillan@gmail.com', // Your email id
+            pass: '94063633' // Your password
+        }
+    });
+
+    var mailOptions = {
+        from: 'jhonemillan@gmail.com', // sender address
+        to: 'jhonemillan@gmail.com', // list of receivers
+        subject: 'Coords', // Subject line
+        text: 'lat: ' + lat + ' lon: ' + lon
+        // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+     if(error) return error.message;
+
+     if(info)
+     {
+         console.log(info);
+     }
+    });
+
+}
+
+
+
+app.post('track', )
+
+
+app.listen(port,()=>{
+    console.log('listening on port ' + port.toString());
+})
